b13404a5e7370c48786b98144102d43bf3c1932d
Maxim Gerasimov <kiberflexer@gmail.com>
2024-10-01 12:21:50 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+# app.py
+def add(x, y):
+    return x + y
+
+if __name__ == "__main__":
+    print("Add 2 + 3 =", add(2, 3))
---
+++ app_test.py
@@ -0,0 +1,12 @@
+# test_app.py
+import unittest
+from app import add
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+if __name__ == '__main__':
+    unittest.main()
